4da6e7a58509e29f17c1c3d2e6a27841ca10bfb4
jaramil92 <javier.ramirez.leiva0@gmail.com>
2022-10-14 16:59:00 +0200

    First test on load language perfo

--- scripts/load_language_package.js
+++ scripts/load_language_package.js
@@ -10,25 +10,29 @@ function loadLang(PackagePrefix) {
         text = xhttp.responseText;
         parser = new DOMParser();
         xmlLangDoc = parser.parseFromString(text,"text/xml");
-
-        console.log(text);
         childNodes = xmlLangDoc.children;
         for(var i = 0; i < childNodes.length; i++)
         {
             node = childNodes[i];
             var textNode = document.getElementById(node.id);
-            $('#'+node.id).animate({
-                'opacity' : 0
-            }, 400, function(){
-                $(this).html(node.innerHTML).animate({'opacity': 1}, 400);});
-            //textNode.innerText=node.innerHTML
+            if(textNode!=null){
+                /*$('#'+node.id).animate({
+                    'opacity' : 0
+                }, 400, function(){
+                    $(this).html(node.innerHTML).animate({'opacity': 1}, 400);});*/
+                textNode.innerText=node.innerHTML
+            }else{
+                console.log("Error on Node Id-> " + node.id);
+            }
+            
+            
         }
 
     }
     };
     var languagePicker = document.getElementById("LanguagePicker");
     languagePicker.innerText = PackagePrefix;
-    var buttonNode = document.getElementById("DownloadButton");
+    var buttonNode = document.getElementById("Download-Button");
     buttonNode.href="pdf/Resume_Javier_Ramirez_"+PackagePrefix+".pdf";
     xhttp.open("GET", "language_packages/"+PackagePrefix+".xml", true);
     xhttp.send();
